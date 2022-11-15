5f13aa6153895f4ae0ab83eb0adbedf90ff36802
nicolarushton <nicola.rushton@codurance.com>
2022-11-15 17:14:26 +0000

    wip - added test for invoking the output writer in the calculator service

--- src/test/java/com/codurance/CalculatorServiceTest.java
+++ src/test/java/com/codurance/CalculatorServiceTest.java
@@ -3,6 +3,7 @@ package com.codurance;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
 
@@ -17,6 +18,19 @@ public class CalculatorServiceTest {
     calculatorService.calculate("1 + 2");
 
     verify(mockOperator, times(1)).execute(1, 2);
+  }
+
+  @Test
+  public void service_invokes_output_writer() {
+
+    Operator mockOperator = mock(Operator.class);
+    OutputWriter mockOutputWriter = mock(OutputWriter.class);
+    when(mockOperator.execute(2,3)).thenReturn(5);
+
+    CalculatorService calculatorService = new CalculatorService(mockOperator);
+    calculatorService.calculate("2 + 3");
+
+    verify(mockOutputWriter, times(1)).printLine("5");
   }
 
 }
