4406da656ab191704c7dc754ac4213af80dffec2
nicolarushton <nicola.rushton@codurance.com>
2022-11-17 16:54:56 +0000

    make concrete parser class

---
+++ src/main/java/com/codurance/TwoOperandParser.java
@@ -0,0 +1,11 @@
+package com.codurance;
+
+import java.util.List;
+
+public class TwoOperandParser implements EquationParser {
+
+  @Override
+  public List<Integer> parse(String s) {
+    throw new UnsupportedOperationException();
+  }
+}
