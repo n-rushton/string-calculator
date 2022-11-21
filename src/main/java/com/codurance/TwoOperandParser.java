aad72f0cebbdb802c96a5b08859bc77c28f8faf1
nicolarushton <nicola.rushton@codurance.com>
2022-11-21 14:49:24 +0000

    refactored String concatenation to StringBuilder

--- src/main/java/com/codurance/TwoOperandParser.java
+++ src/main/java/com/codurance/TwoOperandParser.java
@@ -7,16 +7,16 @@ public class TwoOperandParser implements EquationParser {
   @Override
   public List<Integer> parse(String equation) {
 
-    String firstString = "";
+    StringBuilder firstString = new StringBuilder();
 
     for (int i = 0; i < equation.length(); i++) {
       if (' ' == equation.charAt(i)) {
         break;
       }
-      firstString += equation.charAt(i);
+      firstString.append(equation.charAt(i));
     }
 
-    Integer firstInt = Integer.parseInt(firstString);
+    Integer firstInt = Integer.parseInt(firstString.toString());
 
     char secondChar = equation.charAt(equation.length() - 1);
     String secondString = String.valueOf(secondChar);
