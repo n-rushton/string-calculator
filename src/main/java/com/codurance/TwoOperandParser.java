45cef85b33b9f54552671d53d1e7be136fe6725b
nicolarushton <nicola.rushton@codurance.com>
2022-11-22 16:51:08 +0000

    refactor

--- src/main/java/com/codurance/TwoOperandParser.java
+++ src/main/java/com/codurance/TwoOperandParser.java
@@ -7,27 +7,23 @@ public class TwoOperandParser implements EquationParser {
   @Override
   public List<Integer> parse(String equation) {
 
-    StringBuilder firstString = new StringBuilder();
+    StringBuilder firstString = parseInteger(equation, 0);
+    Integer firstInt = Integer.parseInt(firstString.toString());
 
-    for (int i = 0; i < equation.length(); i++) {
+    StringBuilder secondString = parseInteger(equation, firstString.length() + 3);
+    Integer secondInt = Integer.parseInt(secondString.toString());
+
+    return List.of(firstInt, secondInt);
+  }
+
+  private static StringBuilder parseInteger(String equation, int startingPosition) {
+    StringBuilder firstString = new StringBuilder();
+    for (int i = startingPosition; i < equation.length(); i++) {
       if (' ' == equation.charAt(i)) {
         break;
       }
       firstString.append(equation.charAt(i));
     }
-
-    Integer firstInt = Integer.parseInt(firstString.toString());
-
-    StringBuilder secondString = new StringBuilder();
-
-    for (int i = firstString.length() + 3; i < equation.length(); i++) {
-      if (' ' == equation.charAt(i)) {
-        break;
-      }
-      secondString.append(equation.charAt(i));
-    }
-
-    Integer secondInt = Integer.parseInt(secondString.toString());
-    return List.of(firstInt, secondInt);
+    return firstString;
   }
 }
