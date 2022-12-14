40ed8dedc66e481d8523484d32cdfc9fa6a7e539
nicolarushton <nicola.rushton@codurance.com>
2022-12-14 10:59:14 +0000

    remove hard coded operator symbols from regex expression

--- src/main/java/com/codurance/calculator/parsers/TwoOperandParser.java
+++ src/main/java/com/codurance/calculator/parsers/TwoOperandParser.java
@@ -5,18 +5,25 @@ import com.codurance.calculator.operators.Operator;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class TwoOperandParser implements EquationParser {
 
   private final List<Operator> operators;
+  private final String findOperatorsRegex;
 
   public TwoOperandParser(List<Operator> operators) {
     this.operators = operators;
+    this.findOperatorsRegex = String.format("(?<=\\d)[%s]", getOperatorSymbol(operators));
+  }
+
+  private String getOperatorSymbol(List<Operator> operators) {
+    return operators.stream().map(Operator::getSymbol).collect(Collectors.joining());
   }
 
   private List<Integer> parseIntegers(String equation) {
     String equationWithoutSpaces = equation.replace(" ", "");
-    String[] operands = equationWithoutSpaces.split("(?<=\\d)[+-]");
+    String[] operands = equationWithoutSpaces.split(findOperatorsRegex);
 
     Integer firstInt = Integer.parseInt(operands[0]);
     Integer secondInt = Integer.parseInt(operands[1]);
@@ -33,7 +40,7 @@ public class TwoOperandParser implements EquationParser {
 
   private Operator getOperator(String expression) {
     expression = expression.replace(" ", "");
-    Pattern pattern = Pattern.compile("(?<=\\d)[+-]");
+    Pattern pattern = Pattern.compile(findOperatorsRegex);
     Matcher matcher = pattern.matcher(expression);
 
     if (matcher.find())
