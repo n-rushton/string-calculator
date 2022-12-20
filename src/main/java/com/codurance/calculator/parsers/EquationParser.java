a5bf899db7a533c2aa9209a87eb2c78761728066
tobywoollaston <tobywoollaston@hotmail.co.uk>
2022-12-20 13:11:16 +0000

    remove comment

--- src/main/java/com/codurance/calculator/parsers/EquationParser.java
+++ src/main/java/com/codurance/calculator/parsers/EquationParser.java
@@ -3,7 +3,5 @@ package com.codurance.calculator.parsers;
 import java.util.List;
 
 public interface EquationParser {
-//    List<Integer> parse(String s);
-
     Equation parseExpression(String expression);
 }
