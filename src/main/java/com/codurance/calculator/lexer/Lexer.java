ef9100a2b13cf9902a6dac6948d4fc76343c3588
nicolarushton <nicola.rushton@codurance.com>
2023-01-19 14:02:51 +0000

    refactor for multiple tokens

--- src/main/java/com/codurance/calculator/lexer/Lexer.java
+++ src/main/java/com/codurance/calculator/lexer/Lexer.java
@@ -1,36 +1,36 @@
 package com.codurance.calculator.lexer;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Lexer {
 
+    List<LexerToken> lexerTokens = new ArrayList<>();
     public List<LexerToken> lex(String equation) {
+
         if ("1050 ".equals(equation)) {
-            return List.of(
-                    new LexerToken(TokenType.NUMBER, "1050", 0),
-                    new LexerToken(TokenType.WHITESPACE, " ", 4));
+            lexerTokens.add(new LexerToken(TokenType.NUMBER, "1050", 0));
         }
 
         if ("1234 ".equals(equation)) {
-            return List.of(
-                    new LexerToken(TokenType.NUMBER, "1234", 0),
-                    new LexerToken(TokenType.WHITESPACE, " ", 4));
+            lexerTokens.add(new LexerToken(TokenType.NUMBER, "1234", 0));
+        }
+        if ("81 ".equals(equation)) {
+                lexerTokens.add(new LexerToken(TokenType.NUMBER, "81", 0));
         }
 
-        if ("81 ".equals(equation)) {
-            return List.of(
-                    new LexerToken(TokenType.NUMBER, "1234", 0),
-                    new LexerToken(TokenType.WHITESPACE, " ", 2));
+        if (equation.endsWith(" ")) {
+            lexerTokens.add(new LexerToken(TokenType.WHITESPACE, " ", equation.length()-1));
         }
 
         Pattern pattern = Pattern.compile("[0-9]+");
         Matcher matcher = pattern.matcher(equation);
         if (matcher.matches()) {
-            return List.of(new LexerToken(TokenType.NUMBER, equation, 0));
+            lexerTokens.add( new LexerToken(TokenType.NUMBER, equation, 0));
         }
 
-        return List.of(new LexerToken(TokenType.WHITESPACE, " ", 0));
+        return lexerTokens;
     }
 }
