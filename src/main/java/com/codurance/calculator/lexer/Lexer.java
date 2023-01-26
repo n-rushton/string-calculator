2ca49219e00e4e032e12c5e89c122c479f8f950b
Aman Panesar <amandeep.panesar@codurance.com>
2023-01-26 13:54:05 +0000

    remove nested methods

--- src/main/java/com/codurance/calculator/lexer/Lexer.java
+++ src/main/java/com/codurance/calculator/lexer/Lexer.java
@@ -9,34 +9,22 @@ public class Lexer {
 
     public static final String SPACE = " ";
     List<LexerToken> lexerTokens = new ArrayList<>();
+
     public List<LexerToken> lex(String equation) {
 
         Pattern oneOrMoreNumbersPattern = Pattern.compile("[0-9]+");
         Matcher numberMatcher = oneOrMoreNumbersPattern.matcher(equation);
-        if (numberMatcher.find()){
-            String number = numberMatcher.group(0);
-            isNumberFollowedBySpace(equation, number);
-        }
 
+        if (numberMatcher.find()) {
+            String number = numberMatcher.group(0);
+            lexerTokens.add(new LexerToken(TokenType.NUMBER, number, 0));
+        }
 
         if (equation.endsWith(SPACE)) {
-            lexerTokens.add(new LexerToken(TokenType.WHITESPACE, SPACE, equation.length()-1));
-        }
-
-        if (numberMatcher.matches()) {
-            lexerTokens.add( new LexerToken(TokenType.NUMBER, equation, 0));
+            lexerTokens.add(new LexerToken(TokenType.WHITESPACE, SPACE, equation.length() - 1));
         }
 
         return lexerTokens;
     }
 
-    private void isNumberFollowedBySpace(String equation, String number) {
-        if ((number + SPACE).equals(equation)) {
-            addNumberToken(number);
-        }
-    }
-
-    private void addNumberToken(String number) {
-        lexerTokens.add(new LexerToken(TokenType.NUMBER, number, 0));
-    }
 }
