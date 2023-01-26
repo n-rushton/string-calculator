5b6fbdb74947e1f19afc6e015765ed3fc81063d4
nicolarushton <nicola.rushton@codurance.com>
2023-01-26 13:30:46 +0000

    refactor

--- src/main/java/com/codurance/calculator/lexer/Lexer.java
+++ src/main/java/com/codurance/calculator/lexer/Lexer.java
@@ -12,30 +12,30 @@ public class Lexer {
 
         String space = " ";
         String number1050 = "1050";
-        if ((number1050 + space).equals(equation)) {
-            addNumberToken(number1050);
-        }
+        isNumberFollowedBySpace(equation, space, number1050);
 
         String number1234 = "1234";
-        if ((number1234 + space).equals(equation)) {
-            addNumberToken(number1234);
-        }
+        isNumberFollowedBySpace(equation, space, number1234);
         String number81 = "81";
-        if ((number81 + space).equals(equation)) {
-            addNumberToken(number81);
-        }
+        isNumberFollowedBySpace(equation, space, number81);
 
         if (equation.endsWith(space)) {
             lexerTokens.add(new LexerToken(TokenType.WHITESPACE, space, equation.length()-1));
         }
 
-        Pattern pattern = Pattern.compile("[0-9]+");
-        Matcher matcher = pattern.matcher(equation);
-        if (matcher.matches()) {
+        Pattern oneOrMoreNumbersPattern = Pattern.compile("[0-9]+");
+        Matcher numberMatcher = oneOrMoreNumbersPattern.matcher(equation);
+        if (numberMatcher.matches()) {
             lexerTokens.add( new LexerToken(TokenType.NUMBER, equation, 0));
         }
 
         return lexerTokens;
+    }
+
+    private void isNumberFollowedBySpace(String equation, String space, String number81) {
+        if ((number81 + space).equals(equation)) {
+            addNumberToken(number81);
+        }
     }
 
     private void addNumberToken(String number) {
